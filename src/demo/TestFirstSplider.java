e371c679c5e34192b429ef0c0121a05dc56c1bf0
lili <lili@360.net>
2018-04-10 09:23:43 +0800

    剩余工程文件

--- src/demo/TestFirstSplider.java
+++ src/demo/TestFirstSplider.java
@@ -14,7 +14,7 @@ public class TestFirstSplider implements PageProcessor {
     public void process(Page page) {
         page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
         page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
-        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='public']/strong/a/text()").toString());
         if (page.getResultItems().get("name")==null){
             //skip this page
             page.setSkip(true);
